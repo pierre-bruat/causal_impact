1b14981d7595345f16e9cf7f8ada4acfd8ec1e01
Pierre Bruat <pbruat33@gmail.com>
2022-02-17 14:48:47 +0100

    Update causal_impact.py

--- causal_impact.py
+++ causal_impact.py
@@ -70,10 +70,10 @@ def perform_test_analysis(df, kpi='impressions'):
 st.title("Causal impact launcher")
 with st.expander("settings"):
 	form = st.form(key='my-form')
-	#st.markdown("What file do I need to upload ? Step #1: Export your data from Google Search for your test group and control group. ***** Step #2 Concatenate both files by respecting the following format CSV (;) with following header Date | Clicks | Impressions | CTR | Position | groups (CONTROL or TEST) ")
+	st.markdown("What file do I need to upload ? Step #1: Export your data from Google Search for your test group and control group. ***** Step #2 Concatenate both files by respecting the following format CSV (;) with following header Date | Clicks | Impressions | CTR | Position | groups (CONTROL or TEST) ")
 #image = Image.open(')
-	#kpi = form.selectbox("KPI",("Clicks","Impressions","CTR","Position"))
-	#MEP_DATE = form.text_input("ex: 2022-02-09, please respect this format") 
+	kpi = form.selectbox("KPI",("Clicks","Impressions","CTR","Position"))
+	MEP_DATE = form.text_input("ex: 2022-02-09, please respect this format") 
 	uploaded_file = form.file_uploader("Upload your XLSX file")
 	submit = form.form_submit_button('Submit')
 	if submit:
