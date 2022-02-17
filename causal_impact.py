9ec117435979e30fd310ccd8116221cc0a92fae7
Pierre Bruat <pbruat33@gmail.com>
2022-02-17 10:43:21 +0100

    Create causal_impact.py

---
+++ causal_impact.py
@@ -0,0 +1,94 @@
+import pandas as pd
+from gsc_by_url import gsc_by_url_3
+from collections import defaultdict
+import datetime
+from dateutil import relativedelta
+from date_manip import date_to_str 
+from oauth import authorize_creds, execute_request
+import causal_impact_def
+import advertools as adv
+import searchconsole
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from causalimpact import CausalImpact
+from PIL import Image
+
+
+
+def plot_raw_curves(df, kpi):
+    plt.figure(figsize=(15, 8))
+    df\
+        .set_index(
+            'date', 
+            inplace=True
+        )
+    df\
+        .groupby('groups')[kpi]\
+                            .plot(legend=True)
+                        
+    plt.axvline(
+        pd.to_datetime(MEP_DATE), 
+        color='r', 
+        linestyle='--', 
+        lw=2
+    )
+    plt.title(f"SEO {kpi.capitalize()} for pages of group Control (no change) vs Test (new feature applied)")
+    
+    
+def plot_diff_curve(df, kpi):
+    plt.figure(figsize=(15, 8))
+    pivot_df = df\
+        .reset_index()\
+            .pivot_table(index="date", columns="groups", values=kpi, aggfunc=np.sum)
+    pivot_df["difference"] = pivot_df["TEST"] - pivot_df["CONTROL"]
+    pivot_df = pivot_df.dropna()
+    pivot_df['difference']\
+        .plot(legend=True)
+    plt.axvline(
+        pd.to_datetime(MEP_DATE), 
+        color='r', 
+        linestyle='--', 
+        lw=2
+    )
+    plt.title(f"Difference (Test - Control) of {kpi.capitalize()} between both groups")
+    return pivot_df
+
+
+def compute_causal_impact(pivot_df):
+    change_point = sum(pivot_df.index < pd.to_datetime(MEP_DATE)) - 1
+    pre_period = [0, int(change_point)]
+    post_period = [int(change_point) + 1, len(pivot_df.index) - 1]
+
+    ci = CausalImpact(pivot_df[["difference"]].reset_index(drop=True), pre_period, post_period)
+    print(ci.summary())
+    print(ci.summary(output='report'))
+    ci.plot()
+    
+    
+def perform_test_analysis(df, kpi='impressions'):
+    plot_raw_curves(df, kpi)
+    pivot_df = plot_diff_curve(df, kpi)
+    compute_causal_impact(pivot_df)
+
+
+@st.cache
+
+
+st.title("Causal impact launcher 🤖")
+form = st.form(key='my-form')
+comments.markdown("What file do I need to upload ? Step #1: Export your data from Google Search for your test group and control group. ***** Step #2 Concatenate both files by respecting the following format CSV (;) with following header Date | Clicks | Impressions | CTR | Position | groups (CONTROL or TEST) ")
+#image = Image.open(')
+kpi = form.selectbox("KPI",("Clicks","Impressions","CTR","Position"))
+uploaded_file = form.file_uploader("Upload your CSV file")
+
+submit = form.form_submit_button('Submit')
+if submit:
+		causal_impact = perform_test_analysis(df, kpi=kpi)
+		st.write(causal_impact)
+
+
+
+
+
