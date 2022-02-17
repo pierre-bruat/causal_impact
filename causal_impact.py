d2adce6a3c98912759aa7c30879d58678635c9d1
Pierre Bruat <pbruat33@gmail.com>
2022-02-17 10:51:41 +0100

    Update causal_impact.py

--- causal_impact.py
+++ causal_impact.py
@@ -75,8 +75,7 @@ def perform_test_analysis(df, kpi='impressions'):
 
 @st.cache
 
-
-st.title("Causal impact launcher 🤖")
+header = st.title("Causal impact launcher 🤖")
 form = st.form(key='my-form')
 comments.markdown("What file do I need to upload ? Step #1: Export your data from Google Search for your test group and control group. ***** Step #2 Concatenate both files by respecting the following format CSV (;) with following header Date | Clicks | Impressions | CTR | Position | groups (CONTROL or TEST) ")
 #image = Image.open(')
