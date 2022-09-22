71d081b5d82b42169a243354f97eb85d73d8c46e
Pierre Bruat <35839500+pierre-bruat@users.noreply.github.com>
2022-09-22 14:20:06 +0200

    Update causal_impact.py

--- causal_impact.py
+++ causal_impact.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from causalimpact import CausalImpact
+import warnings
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
